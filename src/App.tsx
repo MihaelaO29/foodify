0ddf04ed1f6a893ace6c7180a892c22df1f58cdf
MihaelaO29 <opriamihaela22@yahoo.com>
2024-05-14 22:01:53 +0300

    added recipe ingredients section and style

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,24 @@ import food_img1 from './img/food1.jpg';
 import food_img2 from './img/food2.jpg';
 import food_img3 from './img/food3.jpg';
 import food_img4 from './img/food4.jpg';
+import time from './img/time.png';
+import user from './img/user.png';
+import plus from './img/plus.png';
+import minus from './img/minus.png';
+import mark from './img/mark.png';
+import check from './img/check.png'
 import axios from 'axios';
 import _, { lastIndexOf } from 'lodash';
 
 function App() {
   const API = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
   const KEY = '3292433c-46b7-40be-bc23-e568af71e2ab';
+
   const itemsPerPage = 10;
   const [search, setSearch] = useState('');
   const [allRecipes, setAllRecipes] = useState<any[]>([]);
   const [page, setPage] = useState<number>(1);
-
+  const [recipesDetails, setReipesDetails] = useState<any>({});
 
   const handleSearchContent = (event: { target: { value: SetStateAction<string>; }; }) => {
     setSearch(event?.target?.value)
@@ -32,6 +39,17 @@ function App() {
     setSearch('')
     if (!_.isEmpty(recipes)) {
       setAllRecipes(recipes);
+    }
+  }
+
+  const fetchRecipesDetails = async (id: string ) => {
+    if (id) {
+      const recipe = await axios
+        .get(`${API}${id}?key=${KEY}`)
+        .then(respone => respone.data)
+        .then(respone => respone.data)
+        .then(respone => respone.recipe)
+      setReipesDetails(recipe)
     }
   }
 
@@ -59,21 +77,21 @@ function App() {
               <div className='search'>
                 <button className='search_btn' onClick={fetchRecipes}>
                   <img src={magnifier} alt='magnifier' /> Search</button>
-                  </div>
+              </div>
 
             </div>
             <div className='bookmark_section'>
-            <div className='bookmark'>
-              <img className='bookmark_img' src={bookmark} alt='bookmark' />
-              <button className='bookmark_btn'> BOOKMARKS </button>
-            </div>
+              <div className='bookmark'>
+                <img className='bookmark_img' src={bookmark} alt='bookmark' />
+                <button className='bookmark_btn'> BOOKMARKS </button>
+              </div>
             </div>
 
           </div>
           <div className='content'>
             <div className='recipes_list'>
               {_.isEmpty(allRecipes) ? (
-                <div className='instructions'>
+                <div className='search_instructions'>
                   <p> Start by searching for a <br /> recipe or an ingredient.</p>
                   <p> Have fun! </p>
                 </div>
@@ -81,7 +99,7 @@ function App() {
                 <div className='recipe_item_list'>
                   <div className='list'>
                     {allRecipes.slice((page - 1) * itemsPerPage, page * itemsPerPage).map((recipe: any) =>
-                      <div className='item_list' key={recipe.id}>
+                      <div onClick={()=>fetchRecipesDetails(recipe.id)} className='item_list' key={recipe.id}>
                         <div className='recipe_product_image'>
                           <img className='product_image' src={recipe.image_url} alt="Content" />
                         </div>
@@ -108,13 +126,53 @@ function App() {
                 </div>
               )}
             </div>
-            
-            <div className='image_section'>
-              <img className='food_image' src={food_img1} alt='food_image' />
-              <img className='food_image' src={food_img2} alt='food_image' />
-              <img className='food_image' src={food_img3} alt='food_image' />
-              <img className='food_image' src={food_img4} alt='food_image' />
-            </div>
+            {(!_.isNil(recipesDetails) && !_.isEmpty(recipesDetails)) ? (
+              <div className='recipe_instructions'>
+                <div
+                  style={{
+                    backgroundImage: `url(${recipesDetails.image_url})`,
+                  }}
+                  className='recipe_image' >
+                  <div className='background_color'></div>
+                  <h1 className='recipe_instructions_title'>
+                    <span className='recipe__title_span'> {recipesDetails.title} </span>
+                  </h1>
+                </div>
+                <div className='recipe_time'>
+                  <div className='time'>
+                    <img src={time} alt='clock_image' /> {recipesDetails.cooking_time} MINUTES
+                  </div>
+                  <div className='recipe_servings'>
+                    <img src={user} alt='user_image' />
+                    {recipesDetails.servings} SERVINGS
+                    <img src={minus} alt='minus_image' />
+                    <img src={plus} alt='plus_image' />
+                  </div>
+                  <div className='instructions_mark'>
+                    <img className='mark' src={mark} alt='instructions_mark' />
+                  </div>
+                </div>
+                <div className='recipe_ingredients_list'>
+                  <p className='ingredients_title'>RECIPE INGREDIENTS</p>
+                  <div className='ingredinet_row'>
+                    {recipesDetails?.ingredients?.map((ingredient: any, index: number) => (
+                      <div className='all_ingredients' key={index}>
+                        <img className='check' src={check} alt="Check icon" /> 
+                        <span className='ingredient'>
+                          {(ingredient.quantity || '') + ' ' + ingredient.unit + ' ' + ingredient.description}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className='image_section'>
+                <img className='food_image' src={food_img1} alt='food_image' />
+                <img className='food_image' src={food_img2} alt='food_image' />
+                <img className='food_image' src={food_img3} alt='food_image' />
+                <img className='food_image' src={food_img4} alt='food_image' />
+              </div>)}
           </div>
         </div>
 
